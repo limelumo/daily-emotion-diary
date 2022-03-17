e0a5cae1f139fe64a04a7cf67c663e8b7aba060d
Lihu Kim <lh.ines.kim@gmail.com>
2022-03-17 17:29:51 +0900

    HOME: Header component

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,30 @@
+import { useState } from 'react';
+
+import DiaryHeader from './../components/DiaryHeader';
+import DiaryButton from './../components/DiaryButton';
+
 const Home = () => {
+  const [curDate, setCurDate] = useState(new Date());
+
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+  const increaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() + 1, curDate.getDate())
+    );
+  };
+  const decreaseMonth = () => {
+    setCurDate(
+      new Date(curDate.getFullYear(), curDate.getMonth() - 1, curDate.getDate())
+    );
+  };
+
   return (
     <div>
-      <h1>Home</h1>
-      <p>이곳은 home입니다</p>
+      <DiaryHeader
+        headText={headText}
+        leftChild={<DiaryButton text={'<'} onClick={decreaseMonth} />}
+        rightChild={<DiaryButton text={'>'} onClick={increaseMonth} />}
+      />
     </div>
   );
 };
