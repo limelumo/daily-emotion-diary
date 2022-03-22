65887f240c0fe5239d03d2a6253d0b8cdd7e11db
Lihu Kim <lh.ines.kim@gmail.com>
2022-03-22 18:43:09 +0900

    feat: 페이지 이동시 title 변경, favicon 설정

--- src/pages/Edit.js
+++ src/pages/Edit.js
@@ -12,6 +12,11 @@ const Edit = () => {
   const diaryList = useContext(DiaryStateContext);
 
   useEffect(() => {
+    const titleElement = document.getElementsByTagName('title')[0];
+    titleElement.innerHTML = `Emotion Diary, edit diary ${id + 1}`;
+  }, []);
+
+  useEffect(() => {
     if (diaryList.length >= 1) {
       const targetDiary = diaryList.find(
         (it) => parseInt(it.id) === parseInt(id)
